cc25d71f4f7d963cd5b9dbfbda746b6f92d21909
kshitij godara <kshitij.godara@nagarro.com>
2019-07-29 20:55:53 +0400

    Inital Commit

---
+++ CoreOcrModule/CoreOcrModule.h
@@ -0,0 +1,19 @@
+//
+//  CoreOcrModule.h
+//  CoreOcrModule
+//
+//  Created by Mac5 on 29/07/19.
+//  Copyright © 2019 Mac5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreOcrModule.
+FOUNDATION_EXPORT double CoreOcrModuleVersionNumber;
+
+//! Project version string for CoreOcrModule.
+FOUNDATION_EXPORT const unsigned char CoreOcrModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreOcrModule/PublicHeader.h>
+
+

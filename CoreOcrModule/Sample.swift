567b943dbdc8358b0d559e0d0206aba9130b5c1b
kshitij godara <kshitij.godara@nagarro.com>
2019-07-29 21:06:36 +0400

    file sample added

---
+++ CoreOcrModule/Sample.swift
@@ -0,0 +1,14 @@
+//
+//  Sample.swift
+//  CoreOcrModule
+//
+//  Created by Mac5 on 29/07/19.
+//  Copyright © 2019 Mac5. All rights reserved.
+//
+
+import Foundation
+class Sample {
+    static func checkPrint() {
+        print("core ocr module")
+    }
+}
